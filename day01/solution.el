f3e00637d42dfe21766be78d71a154a14f111756
Stephen Brennan <stephen@brennan.io>
2020-12-12 11:58:16 -0800

    day01: part 2

--- day01/solution.el
+++ day01/solution.el
@@ -31,23 +31,58 @@
     )
   )
 
+(defun aoc2020/day01/find2-sumto (numvec target start end)
+  "Find two elements of NUMVEC summing to TARGET.
+
+Starting at index START and ending at index END, try to find two elements of the
+sorted vector NUMVEC, which sum to TARGET."
+  (let* ((lhs (aref numvec start))
+         (rhs (aref numvec end))
+         (sum (+ lhs rhs)))
+    (while (and (< start end) (not (= sum target)))
+        (cond ((< sum target) (setq start (+ start 1)))
+              ((> sum target) (setq end (- end 1)))
+        )
+        (setq lhs (aref numvec start))
+        (setq rhs (aref numvec end))
+        (setq sum (+ lhs rhs))
+      )
+    (if (= sum target) (list lhs rhs (* lhs rhs)) nil)
+    )
+  )
+
+(defun aoc2020/day01/find3-sumto (numvec target)
+  "Find three elements of NUMVEC summing to TARGET."
+  (let* ((i 0)
+         (end (- (length numvec) 3))
+         (cur (aref numvec i))
+         (res nil))
+    (while (and (< i end) (not res))
+      (setq res (aoc2020/day01/find2-sumto numvec (- target cur) (+ i 0) (- (length numvec) 1)))
+      (if (not res)
+          (progn (setq i (+ i 1))
+                 (setq cur (aref numvec i)))
+        )
+      )
+    (if res
+        (list cur (elt res 0) (elt res 1) (* cur (elt res 0) (elt res 1)))
+        nil)
+    )
+  )
+
 (defun aoc2020/day01/main (fn)
   "Run solution with FN as input."
   (let* ((numbers (aoc2020/day01/load-ints fn))
+         ;; This is a weird way to create a vector from a list, why do I need to
+         ;; concat here?
          (numvec (vconcat (sort numbers #'<) []))
-         (start 0)
-         (end (- (length numvec) 1)))
-    (while (<= start end)
-      (let* ((lhs (aref numvec start))
-             (rhs (aref numvec end))
-             (sum (+ lhs rhs)))
-        (cond ((= sum 2020) (progn (message "found! %d * %d = %d" lhs rhs (* lhs rhs))
-                                   (setq start (+ start 1))
-                                   (setq end (- end 1))))
-              ((< sum 2020) (setq start (+ start 1)))
-              (t (setq end (- end 1)))
-        ))
-      )
+         (twores (aoc2020/day01/find2-sumto numvec 2020 0 (- (length numvec) 1)))
+         (threeres (aoc2020/day01/find3-sumto numvec 2020)))
+    (if twores
+        (message "Found 2: %d * %d = %d" (elt twores 0) (elt twores 1) (elt twores 2)))
+    (if threeres
+        (message "Found 3: %d * %d * %d = %d"
+                 (elt threeres 0) (elt threeres 1) (elt threeres 2) (elt threeres 3)))
     ))
 
 (aoc2020/day01/main "input.txt")
